724af02511de89752c6c6c044dd223a5e954655f
DENG <908466295@qq.com>
2024-10-21 17:29:50 +0800

    myRes+

--- models/myRes.py
+++ models/myRes.py
@@ -23,6 +23,8 @@ def resnet50_2(weights=None, progress: bool = True, **kwargs: Any):
     if weights is not None:
         model.load_state_dict(weights.get_state_dict(progress=progress), strict=False)
     return model
+
+###########################################################################################################
 ###########################################################################################################
 
 class newBottleneck(Bottleneck):
@@ -243,6 +245,7 @@ class newResNet_2(nn.Module):
     def forward(self, x: Tensor) -> Tensor:
         return self._forward_impl(x)
 ###########################################################################################################
+###########################################################################################################
 
 class blk(nn.Module):
     def __init__(self, in_ch):
@@ -257,12 +260,12 @@ class blk(nn.Module):
         # (160, 512, 224, 224)
         n, c, h, w = x.shape
         # print(x.shape)
-        print(x[:,:c//4].shape, x[:, c//4:c//2].shape, x[:, c//2:c*3//4].shape, x[:, c*3//4:].shape)
+        # print(x[:,:c//4].shape, x[:, c//4:c//2].shape, x[:, c//2:c*3//4].shape, x[:, c*3//4:].shape)
         f1 = self.ce(x[:,:c//4])
         f2 = self.gte(x[:, c//4:c//2])
         f3 = self.lte(x[:, c//2:c*3//4])
         f4 = self.se(x[:, c*3//4:])
-        print(f1.shape, f2.shape, f3.shape, f4.shape)
+        # print(f1.shape, f2.shape, f3.shape, f4.shape)
         x = torch.concat([f1, f2, f3, f4], dim=1)
         return x
 
@@ -407,6 +410,91 @@ class se(nn.Module):
         x = self.bn2(x)
         x = self.sigmoid(x).unsqueeze(2) * res  # (20, 64/g, 8, 224, 224)
         return x.transpose(1, 2).reshape(n, c, h, w)
+
+###########################################################################################################
+###########################################################################################################
+
+class LayerNorm(nn.Module):
+    r""" LayerNorm that supports two data formats: channels_last (default) or channels_first. 
+    The ordering of the dimensions in the inputs. channels_last corresponds to inputs with 
+    shape (batch_size, height, width, channels) while channels_first corresponds to inputs 
+    with shape (batch_size, channels, height, width).
+    """
+    def __init__(self, normalized_shape, eps=1e-6, data_format="channels_last"):
+        super().__init__()
+        self.weight = nn.Parameter(torch.ones(normalized_shape))
+        self.bias = nn.Parameter(torch.zeros(normalized_shape))
+        self.eps = eps
+        self.data_format = data_format
+        if self.data_format not in ["channels_last", "channels_first"]:
+            raise NotImplementedError 
+        self.normalized_shape = (normalized_shape, )
+    
+    def forward(self, x):
+        if self.data_format == "channels_last":
+            return F.layer_norm(x, self.normalized_shape, self.weight, self.bias, self.eps)
+        elif self.data_format == "channels_first":
+            u = x.mean(1, keepdim=True)
+            s = (x - u).pow(2).mean(1, keepdim=True)
+            x = (x - u) / torch.sqrt(s + self.eps)
+            x = self.weight[:, None, None] * x + self.bias[:, None, None]
+            return x
+        
+class GroupGLKA(nn.Module):
+    def __init__(self, n_feats, k=2, squeeze_factor=15):
+        super().__init__()
+        i_feats = 2*n_feats
+        
+        self.n_feats= n_feats
+        self.i_feats = i_feats
+        
+        self.norm = LayerNorm(n_feats, data_format='channels_first')
+        self.scale = nn.Parameter(torch.zeros((1, n_feats, 1, 1)), requires_grad=True)
+        
+        #Multiscale Large Kernel Attention
+        self.LKA7 = nn.Sequential(
+            nn.Conv2d(n_feats//3, n_feats//3, 7, 1, 7//2, groups= n_feats//3),  
+            nn.Conv2d(n_feats//3, n_feats//3, 9, stride=1, padding=(9//2)*4, groups=n_feats//3, dilation=4),
+            nn.Conv2d(n_feats//3, n_feats//3, 1, 1, 0))
+        self.LKA5 = nn.Sequential(
+            nn.Conv2d(n_feats//3, n_feats//3, 5, 1, 5//2, groups= n_feats//3),  
+            nn.Conv2d(n_feats//3, n_feats//3, 7, stride=1, padding=(7//2)*3, groups=n_feats//3, dilation=3),
+            nn.Conv2d(n_feats//3, n_feats//3, 1, 1, 0))
+        self.LKA3 = nn.Sequential(
+            nn.Conv2d(n_feats//3, n_feats//3, 3, 1, 1, groups= n_feats//3),  
+            nn.Conv2d(n_feats//3, n_feats//3, 5, stride=1, padding=(5//2)*2, groups=n_feats//3, dilation=2),
+            nn.Conv2d(n_feats//3, n_feats//3, 1, 1, 0))
+        
+        self.X3 = nn.Conv2d(n_feats//3, n_feats//3, 3, 1, 1, groups= n_feats//3)
+        self.X5 = nn.Conv2d(n_feats//3, n_feats//3, 5, 1, 5//2, groups= n_feats//3)
+        self.X7 = nn.Conv2d(n_feats//3, n_feats//3, 7, 1, 7//2, groups= n_feats//3)
+        
+        self.proj_first = nn.Sequential(
+            nn.Conv2d(n_feats, i_feats, 1, 1, 0))
+
+        self.proj_last = nn.Sequential(
+            nn.Conv2d(n_feats, n_feats, 1, 1, 0))
+
+        
+    def forward(self, x, pre_attn=None, RAA=None):
+        shortcut = x.clone()
+        
+        x = self.norm(x)
+        
+        x = self.proj_first(x)
+        
+        a, x = torch.chunk(x, 2, dim=1) 
+        
+        a_1, a_2, a_3= torch.chunk(a, 3, dim=1)
+        
+        a = torch.cat([self.LKA3(a_1)*self.X3(a_1), self.LKA5(a_2)*self.X5(a_2), self.LKA7(a_3)*self.X7(a_3)], dim=1)
+        
+        x = self.proj_last(x*a)*self.scale + shortcut
+        
+        return x     
+
+###########################################################################################################
+###########################################################################################################
 
 if __name__ == '__main__':
     import torchvision.models as models
